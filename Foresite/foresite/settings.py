dbe7db6bd50ce9af00cdca2af04254174bd3c325
Antonio Jimenez <pricelessantonio@gmail.com>
2018-12-03 17:28:53 -0800

    Unfinished S3 configurations

--- Foresite/foresite/settings.py
+++ Foresite/foresite/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'storages',
+
     'upload_csv',
     'processed_data',
 ]
@@ -134,7 +137,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+AWS_STORAGE_BUCKET_NAME = ''
+AWS_S3_CUSTOM_DOMAIN = ''
+
+AWS_S3_OBJECT_PARAMETErS = {
+    'CacheControl': 'max-age=86400'
+}
+
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'foresite.storage_backends.MediaStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
